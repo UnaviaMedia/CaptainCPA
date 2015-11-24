086a1c6cfa7330ca091e60cf7d64ff650039d788
dougEpp <doug.epp@gmail.com>
2015-11-24 14:58:21 -0500

    Added scene components

---
+++ CaptainCPA/CaptainCPA/Components/MenuComponent.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace CaptainCPA
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class MenuComponent : Microsoft.Xna.Framework.DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+        private SpriteFont regularFont, hilightFont;
+        private List<string> menuItems;
+        private int selectedIndex = 0;
+
+        public int SelectedIndex
+        {
+            get { return selectedIndex; }
+            set { selectedIndex = value; }
+        }
+        private Vector2 position;
+        private Color regularColor = Color.Black;
+        private Color hilightColor = Color.Red;
+
+        private KeyboardState oldState; //Why??
+
+
+        public MenuComponent(Game game,
+            SpriteBatch spriteBatch,
+            SpriteFont regularFont,
+            SpriteFont hilightFont,
+            string[] menuItems)
+            : base(game)
+        {
+            // TODO: Construct any child components here
+            this.spriteBatch = spriteBatch;
+            this.regularFont = regularFont;
+            this.hilightFont = hilightFont;
+            this.menuItems = menuItems.ToList();
+            position = new Vector2(Settings.stage.X / 2, Settings.stage.Y / 2);
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            KeyboardState ks = Keyboard.GetState();
+            if ((ks.IsKeyDown(Keys.Down) && oldState.IsKeyUp(Keys.Down)) || (ks.IsKeyDown(Keys.S) && oldState.IsKeyUp(Keys.S)))
+            {
+                selectedIndex++;
+                if (selectedIndex == menuItems.Count)
+                {
+                    selectedIndex = 0;
+                }
+            }
+            if ((ks.IsKeyDown(Keys.Up) && oldState.IsKeyUp(Keys.Up)) || (ks.IsKeyDown(Keys.W) && oldState.IsKeyUp(Keys.W)))
+            {
+                selectedIndex--;
+                if (selectedIndex == -1)
+                {
+                    selectedIndex = menuItems.Count - 1;
+                }
+            }
+            oldState = ks;
+
+            base.Update(gameTime);
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            Vector2 tempPos = position;
+            spriteBatch.Begin();
+            for (int i = 0; i < menuItems.Count; i++)
+            {
+                if (selectedIndex == i)
+                {
+                    spriteBatch.DrawString(hilightFont, menuItems[i], tempPos, hilightColor);
+                    tempPos.Y += hilightFont.LineSpacing;
+                }
+                else
+                {
+                    spriteBatch.DrawString(regularFont, menuItems[i], tempPos, regularColor);
+                    tempPos.Y += regularFont.LineSpacing;
+                }
+            }
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
--- CaptainCPA/CaptainCPA/Game1.cs
+++ CaptainCPA/CaptainCPA/Game1.cs
@@ -17,8 +17,24 @@ namespace CaptainCPA
 	public class Game1 : Microsoft.Xna.Framework.Game
 	{
 		GraphicsDeviceManager graphics;
-		SpriteBatch spriteBatch;
+        SpriteBatch spriteBatch;
 
+        //scene declaration
+        private StartScene startScene;
+        private ActionScene actionScene;
+
+        private void hideAllScenes()
+        {
+            GameScene gs = null;
+            foreach (GameComponent item in Components)
+            {
+                if (item is GameScene)
+                {
+                    gs = (GameScene)item;
+                    gs.hide();
+                }
+            }
+        }
 		public Game1()
 		{
 			graphics = new GraphicsDeviceManager(this);
@@ -32,8 +48,9 @@ namespace CaptainCPA
 		/// and initialize them as well.
 		/// </summary>
 		protected override void Initialize()
-		{
-			// TODO: Add your initialization logic here
+        {
+            Settings.stage = new Vector2(graphics.PreferredBackBufferWidth,
+                graphics.PreferredBackBufferHeight);
 
 			base.Initialize();
 		}
@@ -43,9 +60,18 @@ namespace CaptainCPA
 		/// all of your content.
 		/// </summary>
 		protected override void LoadContent()
-		{
-			// Create a new SpriteBatch, which can be used to draw textures.
-			spriteBatch = new SpriteBatch(GraphicsDevice);
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //create all scenes and add to the Components list
+            startScene = new StartScene(this, spriteBatch);
+            Components.Add(startScene);
+
+            actionScene = new ActionScene(this, spriteBatch);
+            this.Components.Add(actionScene);
+
+            startScene.show();
 
 			// TODO: use this.Content to load your game content here
 		}
@@ -65,14 +91,61 @@ namespace CaptainCPA
 		/// </summary>
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update(GameTime gameTime)
-		{
-			// Allows the game to exit
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-				this.Exit();
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
 
-			// TODO: Add your update logic here
+            // TODO: Add your update logic here
+            int selectedIndex = 0;
+            KeyboardState ks = Keyboard.GetState();
 
-			base.Update(gameTime);
+            if (startScene.Enabled)
+            {
+                selectedIndex = startScene.Menu.SelectedIndex;
+                if (selectedIndex == 0 && ks.IsKeyDown(Keys.Enter))
+                {
+                    hideAllScenes();
+                    actionScene.show();
+                }
+                //if (selectedIndex == 1 && ks.IsKeyDown(Keys.Enter))
+                //{
+                //    hideAllScenes();
+                //    helpScene.show();
+                //}
+                //if (selectedIndex == 2 && ks.IsKeyDown(Keys.Enter))
+                //{
+                //    hideAllScenes();
+                //    //highScoreScene.show();
+                //}
+                //if (selectedIndex == 3 && ks.IsKeyDown(Keys.Enter))
+                //{
+                //    hideAllScenes();
+                //    //aboutCreditScene.show();
+                //}
+                //if (selectedIndex == 4 && ks.IsKeyDown(Keys.Enter))
+                //{
+                //    hideAllScenes();
+                //    //howToPlayScene.show();
+                //}
+                //... other scenes here
+
+                if (selectedIndex == 5 && ks.IsKeyDown(Keys.Enter))
+                {
+                    Exit();
+                }
+            }
+
+            if (actionScene.Enabled)
+            {
+                if (ks.IsKeyDown(Keys.Escape))
+                {
+                    hideAllScenes();
+                    startScene.show();
+                }
+            }
+
+            base.Update(gameTime);
 		}
 
 		/// <summary>
