73dc3bcbc508ba8b207d861d985f6ec43a844adb
Kendall Roth <kroth@unaviamedia.ca>
2015-11-24 22:59:49 -0500

    Added string to color Converter

---
+++ CaptainCPA/CaptainCPA/Utilities/ColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace CaptainCPA
+{
+	public static class ColorConverter
+	{
+		public static Dictionary<string, Color> TileColors = new Dictionary<string, Color>()
+		{
+			{ "White", Color.White },
+			{ "Red", Color.Red },
+			{ "Green", Color.Green },
+			{ "Blue", Color.Blue },
+			{ "LightRed", Color.PaleVioletRed },
+			{ "LightGreen", Color.LightGreen },
+			{ "LightBlue", Color.LightBlue }
+		};
+
+		public static Color ConvertColor(string input)
+		{
+			try
+			{
+				return TileColors[input];
+			}
+			catch (Exception)
+			{
+				return Color.White;
+			}
+		}
+	}
+}
