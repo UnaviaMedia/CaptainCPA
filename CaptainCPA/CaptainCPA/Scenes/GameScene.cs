78658886d9148b233adaacc10bbc2e034d2552d5
Kendall Roth <kroth@unaviamedia.ca>
2015-11-24 22:33:28 -0500

    Added SpriteBatch property

--- CaptainCPA/CaptainCPA/Scenes/GameScene.cs
+++ CaptainCPA/CaptainCPA/Scenes/GameScene.cs
@@ -12,80 +12,85 @@ using Microsoft.Xna.Framework.Media;
 
 namespace CaptainCPA
 {
-    /// <summary>
-    /// This is a game component that implements IUpdateable.
-    /// </summary>
-    public class GameScene : Microsoft.Xna.Framework.DrawableGameComponent
-    {
-        private List<GameComponent> components;
+	/// <summary>
+	/// This is a game component that implements IUpdateable.
+	/// </summary>
+	public class GameScene : DrawableGameComponent
+	{
+		protected SpriteBatch spriteBatch;
+		protected List<GameComponent> components;
 
-        public List<GameComponent> Components
-        {
-            get { return components; }
-            set { components = value; }
-        }
+		public List<GameComponent> Components
+		{
+			get { return components; }
+			set { components = value; }
+		}
 
-        public virtual void show()
-        {
-            this.Enabled = true;
-            this.Visible = true;
-        }
-        public virtual void hide()
-        {
-            this.Enabled = false;
-            this.Visible = false;
-        }
-        public GameScene(Game game)
-            : base(game)
-        {
-            components = new List<GameComponent>();
-            hide();
-        }
+		public virtual void Show()
+		{
+			this.Enabled = true;
+			this.Visible = true;
+		}
 
-        /// <summary>
-        /// Allows the game component to perform any initialization it needs to before starting
-        /// to run.  This is where it can query for any required services and load content.
-        /// </summary>
-        public override void Initialize()
-        {
-            // TODO: Add your initialization code here
+		public virtual void Hide()
+		{
+			this.Enabled = false;
+			this.Visible = false;
+		}
 
-            base.Initialize();
-        }
+		public GameScene(Game game, SpriteBatch spriteBatch)
+			: base(game)
+		{
+			this.spriteBatch = spriteBatch;
+			components = new List<GameComponent>();
+			Hide();
+		}
 
-        /// <summary>
-        /// Allows the game component to update itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        public override void Update(GameTime gameTime)
-        {
-            foreach (GameComponent item in components)
-            {
-                if (item.Enabled)
-                {
-                    item.Update(gameTime);
-                }
-            }
+		/// <summary>
+		/// Allows the game component to perform any initialization it needs to before starting
+		/// to run.  This is where it can query for any required services and load content.
+		/// </summary>
+		public override void Initialize()
+		{
+			// TODO: Add your initialization code here
 
-            base.Update(gameTime);
-        }
-        public override void Draw(GameTime gameTime)
-        {
-            DrawableGameComponent comp = null;
-            foreach (GameComponent item in components)
-            {
-                if (item is DrawableGameComponent)
-                {
-                    comp = (DrawableGameComponent)item;
-                    if (comp.Visible)
-                    {
-                        comp.Draw(gameTime);
-                    }
+			base.Initialize();
+		}
 
-                }
-            }
+		/// <summary>
+		/// Allows the game component to update itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		public override void Update(GameTime gameTime)
+		{
+			foreach (GameComponent item in components)
+			{
+				if (item.Enabled)
+				{
+					item.Update(gameTime);
+				}
+			}
 
-            base.Draw(gameTime);
-        }
-    }
+			base.Update(gameTime);
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			DrawableGameComponent comp = null;
+			foreach (GameComponent item in components)
+			{
+				if (item is DrawableGameComponent)
+				{
+					comp = (DrawableGameComponent)item;
+					if (comp.Visible)
+					{
+						comp.Draw(gameTime);
+					}
+
+				}
+			}
+
+			base.Draw(gameTime);
+		}
+	}
 }
