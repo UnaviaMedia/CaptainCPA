f6e00547fd8f4846aace3fc623bfd0226c9d0942
UnaviaMedia <kroth@unaviamedia.ca>
2015-11-26 15:56:58 -0500

    Added Directions.cs

---
+++ CaptainCPA/CaptainCPA/Utilities/Directions.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Project: CaptainCPA - Directions.cs
+ * Purpose: Enumeration of different directions
+ *
+ * History:
+ *		Kendall Roth	Nov-26-2015:	Created
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CaptainCPA
+{
+	/// <summary>
+	/// Enumeration of different Tile types
+	/// </summary>
+	public enum Direction
+	{
+		Up, Right, Down, Left
+	}
+}
