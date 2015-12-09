ab064d5822e97a108403a6da156d56075dbd4c1b
UnaviaMedia <kroth@unaviamedia.ca>
2015-12-09 01:13:28 -0500

    Updated HowToPlay with new User Interface design

--- CaptainCPA/CaptainCPA/Scenes/HowToPlayScene.cs
+++ CaptainCPA/CaptainCPA/Scenes/HowToPlayScene.cs
@@ -12,48 +12,54 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace CaptainCPA
 {
-    /// <summary>
-    /// This is a game scene which displays gameplay instruction from a text file
-    /// </summary>
-    public class HowToPlayScene : GameScene
-    {
-        private string message;
-        private SpriteFont font;
-        public HowToPlayScene(Game game, SpriteBatch spriteBatch)
-            : base(game, spriteBatch)
-        {
-            font = game.Content.Load<SpriteFont>("Fonts/MenuFont");
-            message = readFile(@"Text/HowToPlayMessage.txt");
-        }
+	/// <summary>
+	/// This is a game scene which displays gameplay instruction from a text file
+	/// </summary>
+	public class HowToPlayScene : GameScene
+	{
+		private Texture2D menuImage;
 
-        /// <summary>
-        /// Allows the game component to perform any initialization it needs to before starting
-        /// to run.  This is where it can query for any required services and load content.
-        /// </summary>
-        public override void Initialize()
-        {
-            // TODO: Add your initialization code here
+		//private string message;
+		//private SpriteFont font;
 
-            base.Initialize();
-        }
 
-        /// <summary>
-        /// Allows the game component to update itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        public override void Update(GameTime gameTime)
-        {
-            // TODO: Add your update code here
+		public HowToPlayScene(Game game, SpriteBatch spriteBatch)
+			: base(game, spriteBatch)
+		{
+			//font = game.Content.Load<SpriteFont>("Fonts/MenuFont");
+			//message = readFile(@"Text/HowToPlayMessage.txt");
+			menuImage = game.Content.Load<Texture2D>("Images/ControlsMenu");
+		}
 
-            base.Update(gameTime);
-        }
-        public override void Draw(GameTime gameTime)
-        {
-            spriteBatch.Begin();
-            spriteBatch.DrawString(font, message, new Vector2(Settings.TILE_SIZE * 13, Settings.TILE_SIZE * 6), Color.White);
-            spriteBatch.End();
+		/// <summary>
+		/// Allows the game component to perform any initialization it needs to before starting
+		/// to run.  This is where it can query for any required services and load content.
+		/// </summary>
+		public override void Initialize()
+		{
+			// TODO: Add your initialization code here
 
-            base.Draw(gameTime);
-        }
-    }
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// Allows the game component to update itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		public override void Update(GameTime gameTime)
+		{
+			// TODO: Add your update code here
+
+			base.Update(gameTime);
+		}
+		public override void Draw(GameTime gameTime)
+		{
+			spriteBatch.Begin();
+			//spriteBatch.DrawString(font, message, new Vector2(Settings.TILE_SIZE * 13, Settings.TILE_SIZE * 6), Color.White);
+			spriteBatch.Draw(menuImage, Vector2.Zero, Color.White);
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
 }
